5abb5f086a161fdaf6bfdc8549f603bbb6aa4b5b
bennywwg <bennywwg@gmail.com>
2017-08-23 00:17:20 -0500

    started messing around with sql queries

--- abs/abs/Program.cs
+++ abs/abs/Program.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace abs {
     class Program {
         static void Main(string[] args) {
+            string connectionString = "Server=localhost;Port=5432;Username=postgres;Password=biggears;Database=postgres";
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine(connection.State);
+            printAll(connection, "users");
+            
+            Console.ReadKey();
+        }
+
+        static void executeCommand(NpgsqlConnection connection, string query) {
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            command.Dispose();
+        }
+
+        static void printAll(NpgsqlConnection connection, string table) {
+            NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM " + table, connection);
+            NpgsqlDataReader reader = command.ExecuteReader();
+            while (reader.Read()) Console.WriteLine(reader.GetString(0) + "," + reader.GetString(1));
+            reader.Close();
+            command.Dispose();
         }
     }
 }
