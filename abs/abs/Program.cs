5f5ed5e5a94c0ae39923c647a8b8d7e5d7ab25d7
quinn-stp <quinn.stp@gmail.com>
2017-08-29 02:53:16 -0500

    monopage template work

--- abs/abs/Program.cs
+++ abs/abs/Program.cs
@@ -4,31 +4,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Npgsql;
+using monopage;
+using System.Globalization;
 
 namespace abs {
     class Program {
         static void Main(string[] args) {
-            string connectionString = "Server=localhost;Port=5432;Username=postgres;Password=biggears;Database=postgres";
-            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
-            connection.Open();
-            Console.WriteLine(connection.State);
-            printAll(connection, "users");
-            
+            Server server = new Server();
+            mpBase mpBase = new mpBase();
+
+            server.start(mpBase.restful, "http://*:8080/");
+            mpBase.addProperty("test", "the value at test");
+
+            mpPageAssembler assembler = new mpPageAssembler(
+                new mpBetterFile("../../Web/Templates/Template.html"),
+                new mpBetterFile("../../Web/Templates/Template.js"),
+                new mpBetterFile("../../Web/Templates/Template.css"),
+                new mpPageElement("../../Web/Header"),
+                new mpPageElement("../../Web/Footer")
+            );
+            assembler.addContent("test", new mpPageElement("../../Web/Content"));
+            assembler.addContent("test", new mpPageElement("../../Web/Content"));
+            assembler.addContent("test", new mpPageElement("../../Web/Content"));
+            assembler.addContent("test", new mpPageElement("../../Web/Content"));
+
+
+            mpBase.addProperty("web", assembler);
+
             Console.ReadKey();
+
+            server.stop();
+        }
+    }
+
+    class mpBetterFile : mpReadonlyEndpoint {
+        public bool OK { get; private set; }
+        public readonly string location;
+        private binaryData cache;
+        public TimeSpan cacheTimeout;
+        private DateTime lastLoaded;
+
+        public override string tabbedJson(int tab, bool tabFirstLine) {
+            return data.toJson();
+        }
+        public override mpObject meta {
+            get {
+                return new mpObject(
+                    new mpProperty("type", cache.type.ToString()),
+                    new mpProperty("OK", OK),
+                    new mpProperty("lastLoaded", location == null ? DateTime.UtcNow.ToString(DateTimeFormatInfo.InvariantInfo) : lastLoaded.ToString(DateTimeFormatInfo.InvariantInfo))
+                );
+            }
+        }
+        public override binaryData data {
+            get {
+                if (location != null && DateTime.UtcNow - lastLoaded > cacheTimeout) {
+                    load();
+                }
+                return cache;
+            }
+        }
+        public override mpResponse response {
+            get {
+                if (OK) {
+                    return new mpResponse(data, 200);
+                } else {
+                    return new mpResponse(500);
+                }
+            }
         }
 
-        static void executeCommand(NpgsqlConnection connection, string query) {
-            NpgsqlCommand command = new NpgsqlCommand(query, connection);
-            command.ExecuteNonQuery();
-            command.Dispose();
+        private void load() {
+            if (location != null) {
+                lastLoaded = DateTime.UtcNow;
+                try {
+                    cache = binaryData.fromFile(location);
+                    OK = true;
+                } catch {
+                    cache = new binaryData("");
+                    OK = false;
+                }
+            }
         }
 
-        static void printAll(NpgsqlConnection connection, string table) {
-            NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM " + table, connection);
-            NpgsqlDataReader reader = command.ExecuteReader();
-            while (reader.Read()) Console.WriteLine(reader.GetString(0) + "," + reader.GetString(1));
-            reader.Close();
-            command.Dispose();
+        public mpBetterFile(binaryData data) {
+            this.cache = data;
+            this.location = null;
+            this.lastLoaded = DateTime.UtcNow;
+            this.cacheTimeout = TimeSpan.Zero;
+            this.OK = true;
+        }
+        public mpBetterFile(string location) : this(location, new TimeSpan(0,0,5)) {
+        }
+        public mpBetterFile(string location, TimeSpan cacheTimeout) {
+            this.location = location;
+            this.cacheTimeout = cacheTimeout;
+            load();
         }
     }
 }
