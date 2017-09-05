0be9d4ed361641a2bb3379f5d76580698752725c
jake Griesmaier <jakegriesmaier@gmail.com>
2017-09-05 17:23:53 -0500

    changes to Exercises:

---
+++ abs/abs/Exercise.cs
@@ -0,0 +1,81 @@
+﻿namespace abs {
+    public class Exercise {
+
+        public string exerciseName {
+            get; private set;
+        }
+        private BodyPart mainBodyPart;
+        public string specifiedArea {
+            get {
+                if (mainBodyPart == BodyPart.Chest) {
+                    if (areaNumber == 1) return "Middle Chest";
+                    else if (areaNumber == 2) return "Upper Chest";
+                    else return "Lower Chest";
+                }
+                else if (mainBodyPart == BodyPart.Legs) {
+                    if (areaNumber == 1) return "Quads";
+                    else if (areaNumber == 2) return "Hamstrings";
+                    else return "Calves";
+                }
+                else if (mainBodyPart == BodyPart.Back) {
+                    if (areaNumber == 1) return "Upper Back";
+                    else if (areaNumber == 2) return "Lats";
+                    else return "Lower Back";
+                }
+                else if (mainBodyPart == BodyPart.Shoulders) {
+                    if (areaNumber == 1) return "Front Deltiods";
+                    else if (areaNumber == 2) return "Lateral Deltoids";
+                    else return "Rear Deltoids/Traps";
+                }
+                else if (mainBodyPart == BodyPart.Arms) {
+                    if (areaNumber == 1) return "Biceps";
+                    else if (areaNumber == 2) return "Triceps";
+                    else return "Forearms";
+                }
+                else {
+                    if (areaNumber == 1) return "Upper Abs";
+                    else if (areaNumber == 2) return "Lower Abs";
+                    else return "Obliques";
+                }
+                
+
+            }
+        }
+        private LiftType liftType;
+        private int areaNumber;
+        private bool requiresWeight;
+        public int[] reps {
+            get; private set;
+        }
+        public int sets {
+            get; private set;
+        }
+        private string style;
+        public int[] weights {
+            get; private set;
+        }
+        public string equipment {
+            get; private set;
+        }
+
+        public Exercise() {
+
+        }
+
+        public enum BodyPart {
+            Chest = 1, 
+            Legs = 2,
+            Back = 3,
+            Shoulders = 4, 
+            Arms = 5,
+            Abs = 6
+        }
+
+        public enum LiftType {
+            Compound = 1,
+            Isolation = 2
+        }
+
+    }
+
+}
--- abs/abs/WorkoutGenerator.cs
+++ abs/abs/WorkoutGenerator.cs
@@ -50,20 +50,6 @@ namespace abs {
             }
         }
 
-        //make this so that it sorts the list for...
-        public void sortExercises(int[] value) {
-            Boolean keep = false;
-            foreach (Exercise e in Exercises) {
-                for (int i = 0; i < equipmentAvailable.Length; i++) {
-                    if (e.getEquipment() == equipmentAvailable[i]) {
-                        keep = true;
-                    }
-                }
-                if (keep == false) removeExercise(e);
-                keep = false;
-            }
-        }
-
         public void addExercise(Exercise exercise) {
             Exercises.Add(exercise);
             numExercises++;
@@ -76,104 +62,6 @@ namespace abs {
 
         public List<Exercise> getExercises() {
             return Exercises;
-        }
-    }
-
-    //change the getters and setters so that they are accessing the PostgreSQL database
-    public class Exercise {
-
-        private string exerciseName;
-        private string mainBodyPart;
-        private string specifiedArea;
-        private string liftType;
-        private int areaNumber;
-        private bool requiresWeight;
-        private int[] reps;
-        private int sets;
-        private string style;
-        private int[] weights;
-        private string equipment;
-
-
-        public Exercise() { }
-
-        public Exercise(string mainBodyPart, string specifiedArea, string liftType, int areaNumber, bool requiresWeight, string exerciseName, int[] reps, int sets, string style, int[] weights, string equipmemnt) {
-            this.exerciseName = exerciseName;
-            this.mainBodyPart = mainBodyPart;
-            this.liftType = liftType;
-            this.areaNumber = areaNumber;
-            this.specifiedArea = specifiedArea;
-            this.requiresWeight = requiresWeight;
-            this.reps = reps;
-            this.sets = sets;
-            this.style = style;
-            this.weights = weights;
-            this.equipment = equipmemnt;
-        }
-
-        public string getMainBodyPart() {
-            return this.mainBodyPart;
-        }
-
-        public void setMainBodyPart(string mainBodyPart) {
-            this.mainBodyPart = mainBodyPart;
-        }
-
-        public string getspecifiedArea() {
-            return this.specifiedArea;
-        }
-
-        //TODO ADD MORE SETTERS AND GETTERS
-        public void setSpecifiedArea(string specifiedArea) {
-            this.specifiedArea = specifiedArea;
-        }
-
-        public string getExerciseName() {
-            return this.exerciseName;
-        }
-
-        public void setExerciseName(string exerciseName) {
-            this.exerciseName = exerciseName;
-        }
-
-        public int getSets() {
-            return this.sets;
-        }
-
-        public void setSets(int sets) {
-            this.sets = sets;
-        }
-
-        public int[] getReps() {
-            return this.reps;
-        }
-
-        public void setReps(int[] reps) {
-            this.reps = reps;
-        }
-
-        public int[] getWeights() {
-            return this.weights;
-        }
-
-        public void setWeights(int[] weights) {
-            this.weights = weights;
-        }
-
-        public string getStyle() {
-            return this.style;
-        }
-
-        public void setStyle(string style) {
-            this.style = style;
-        }
-
-        public void setEquipment(string equipment) {
-            this.equipment = equipment;
-        }
-
-        public string getEquipment() {
-            return this.equipment;
         }
     }
 
