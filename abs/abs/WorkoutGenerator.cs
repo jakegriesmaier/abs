b89daff2f2e4c4b206ff8abce0a9439c9fcded1d
jake Griesmaier <jakegriesmaier@gmail.com>
2017-09-17 23:15:48 -0500

    changes to workout generator

--- abs/abs/WorkoutGenerator.cs
+++ abs/abs/WorkoutGenerator.cs
@@ -6,68 +6,65 @@ using System.Threading.Tasks;
 using System.Linq;
 
 namespace abs {
+    //experience level, gender, workout length, workout times per week, goal
+
+
+
     //TODO finish
     public class Plan {
-        private static string[] equipmentAvailable = { "Bench", "Dumbbells" };
-        //ExerciseList exercises = new ExerciseList(1, equipmentAvailable);
-        ExerciseList exercises = new ExerciseList();
+        private static string[] equipmentAvailable = { "Bench", "Dumbbells" };//should be user input
+        private static string[] equipmentAvailable2 = { "Bench", "Dumbbells" };//should be user input
+        private static string[] weights = { "Bench", "Dumbbells" };//should be user input
+        private int experienceLevel;
+        private bool gender;//male = true;
+        public int workoutLength;//30, 45, 60, 75, 90, 105, 120 (minutes)
+        public int workoutsPerWeek;//2,3,4,5, or 6
+        public int goal;//1=gain muscle, 2=lose fat, 3=maintain weight
+
+
+        UsersPossibleExerciseList exercises = new UsersPossibleExerciseList();
         List<Exercise> myExercises = new List<Exercise>();
 
-        public Plan() {
-            exercises.sortExercisesByEquipment(equipmentAvailable);
-            myExercises = exercises.getExercises();
+        public Plan(int experienceLevel, bool gender, int workoutLength, int workoutsPerWeek, int goal) {
+            //exercises.sortExercisesByEquipment(equipmentAvailable, equipmentAvailable2, weights);
+            //myExercises = exercises.getExercises();
+            this.experienceLevel = experienceLevel;
+            this.gender = gender;
+            this.workoutLength = workoutLength;
+            this.workoutsPerWeek = workoutsPerWeek;
+            this.goal = goal;
         }
 
+        //selects the style of workout for the plan 
+        public string selectStyle(int experienceLevel) {
+            throw new NotImplementedException();
+        }
+
+        //determines the number of exercises that the plan will contain (based on the workout length)
+        public int numExercises() {
+            throw new NotImplementedException();
+        }
 
     }
 
-    public class ExerciseList {
+
+    //TODO create a list of possible exercises based on what equipment the user has avaiable 
+    public class UsersPossibleExerciseList {
 
         static List<Exercise> Exercises = new List<Exercise>();
-        private int experienceLevel;
         private string exerciseName;
-        private int[] reps;
-        private int sets;
-        private string style;
-        private int[] weights;
-        private string equipment;
         private int numExercises;
 
 
-        public ExerciseList() {
+        public UsersPossibleExerciseList() {
             this.numExercises = 0;
         }
 
-        //TODO find more elegant way to do this
-        public string generateAllExerciseInformationHTMLString() {
-            string exerciseInformation = "<table><tr><th>Exercise</th><th>Specified Area</th><th>Reps</th><th>Sets</th><th>Weights</th><th>Equipment Required</th></tr><tr>";
-            foreach(Exercise e in Exercises) {
-                exerciseInformation += "<td>" + e.exerciseName + "</td>";
-            }
-            exerciseInformation += "</tr><tr>";
-            foreach (Exercise e in Exercises) {
-                exerciseInformation += "<td>" + e.specifiedArea + "</td>";
-            }
-            exerciseInformation += "</tr><tr>";
-            foreach (Exercise e in Exercises) {
-                exerciseInformation += "<td>" + string.Join(",", e.reps) + "</td>";
-            }
-            exerciseInformation += "</tr><tr>";
-            foreach (Exercise e in Exercises) {
-                exerciseInformation += "<td>" + e.sets.ToString() + "</td>";
-            }
-            exerciseInformation += "</tr><tr>";
-            foreach (Exercise e in Exercises) {
-                exerciseInformation += "<td>" + string.Join(",", e.sets) + "</td>";
-            }
-            exerciseInformation += "</tr><tr>";
-            foreach (Exercise e in Exercises) {
-                exerciseInformation += "<td>" + e.equipment + "</td>";
-            }
-            return exerciseInformation += "</tr></table>"; ;
+        public string exercisesAsHTML() {
+            throw new NotImplementedException();
         }
 
-        public void sortExercisesByEquipment(string[] equipmentAvailable) {
+        public void sortExercisesByEquipment(string[] equipmentAvailable, string[] equipmentAvailable2, string[] weights) {
             throw new NotImplementedException();
         }
 
